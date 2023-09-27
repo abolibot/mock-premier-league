a1f10eb459333dbd02ad46801cdbf8a4b381891c
Oluwatobi Alaran <alarantobiloba@gmail.com>
2023-09-27 15:18:34 +0100

    chore: setup redis and mongoose

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import { RedisOptions } from "ioredis";
 
 dotenv.config();
 
@@ -7,3 +8,12 @@ export const port: number = parseInt(process.env.PORT ?? "3000");
 
 export const allowedDomains: string[] =
     process.env.ALLOWED_DOMAINS?.split(",") ?? [];
+
+export const redisConfiguration: RedisOptions = {
+    host: process.env.REDIS_HOST!,
+    port: Number(process.env.REDIS_PORT),
+    username: process.env.REDIS_USERNAME ?? "",
+    password: process.env.REDIS_PASSWORD ?? "",
+};
+
+export const mongoUri: string = process.env.MONGO_URI!;
